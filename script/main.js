7f110046f478344e225b138fe42af935b54562f0
Sonat <80075444+sonatipek@users.noreply.github.com>
2022-07-26 20:12:20 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("#form-submit");
+const resim = document.getElementById("#info-img");
+const name = document.getElementById("#name");
+const email = document.getElementById("#email");
+const message = document.getElementById("#message");
+
+function resimDegistir(){
+  
+   resim.src='/images/mail_sent.svg';
+  
+}
